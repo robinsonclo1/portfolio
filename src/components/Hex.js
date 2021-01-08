73b5f32473206b1756afefc9eb7a77aebd7aedae
Robin <robin.clower@workstate.com>
2021-01-08 09:53:07 -0500

    Added font-awesome elements

--- src/components/Hex.js
+++ src/components/Hex.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import './Hex.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faWheelchair, faDice, faMicrophoneAlt, faToolbox, faAddressCard } from '@fortawesome/free-solid-svg-icons'
 
 class Hex extends React.Component {
   constructor() {
@@ -9,7 +11,6 @@ class Hex extends React.Component {
       hovered: false
     }
   }
-  
 
   handleChange() {
     this.props.setActiveHex(this.props.size);
@@ -20,10 +21,44 @@ class Hex extends React.Component {
   
   render() {
     return (
-      <div className={this.state.hovered ? 'hex-lg' :  this.props.size} onMouseOver={this.handleChange} onMouseLeave={this.handleChange}>
+      <div className={this.state.hovered ? 'hex-lg' :  this.props.size} onMouseEnter={this.handleChange} onMouseLeave={this.handleChange}>
+        <div className={'hex-interior'}>
+          {/*<FontAwesomeIcon icon={faWheelchair}/>*/}
+          {/*{this.state.hovered &&*/}
+          {/*  <>*/}
+          {/*    <button>Hello There!</button>*/}
+          {/*  </>*/}
+          {/*}*/}
+        </div>
+        {/*<FontAwesomeIcon icon={faWheelchair} />*/}
+        {/*<FontAwesomeIcon icon={faDice} />*/}
+        {/*<FontAwesomeIcon icon={faMicrophoneAlt} />*/}
+        {/*<FontAwesomeIcon icon={faToolbox} />*/}
+        {/*<FontAwesomeIcon icon={faAddressCard} />*/}
       </div>
     )
   }
 }
 
 export default Hex
+
+// Accessible faWheelchair
+// Teacher/Speaker faMicrophoneAlt
+// Adaptable faToolbox
+// Hobbies faDice
+// Contact Me faAddressCard
+//
+
+//other fun ones:
+// brain
+// battery full
+// code / codebranch / cloud
+// fileUpload / folderPlus
+
+// json structure
+// row
+//   column
+//     title
+//     fontAwesomeIcon
+//     exec summary
+//     full summary
