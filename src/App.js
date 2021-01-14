7920c4fa844bfeced914e67936c0cacd3a83b6bc
Robin <robin.clower@workstate.com>
2021-01-14 11:08:00 -0500

    Added modal support, still need to do some more styling and fix copy

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import './App.scss';
 import React from 'react'
 import HexRow from './components/HexRow'
-import hexContent from "./hexContent"
+import hexContent from './hexContent'
+import Modal from './components/Modal'
 
 class App extends React.Component {
   constructor() {
     super()
+    this.toggleModal = this.toggleModal.bind(this)
     this.setActiveHex = this.setActiveHex.bind(this)
     this.state = {
       inverted: false,
-      content: hexContent
+      content: hexContent,
+      showModal: false,
+      activeInfo: '',
+      icon: ''
     }
   }
   
@@ -17,6 +22,15 @@ class App extends React.Component {
     if (size === 'hex-sm') {
       this.toggleInversion()
     }
+  }
+  
+  toggleModal(item, icon) {
+    console.log(item, icon)
+    this.setState({
+      showModal: !this.state.showModal,
+      activeInfo: item,
+      icon: icon
+    })
   }
   
   toggleInversion = () => {
@@ -27,7 +41,9 @@ class App extends React.Component {
   
   render() {
     const hexRow = this.state.content.map((item, i) =>
-      <HexRow inverted={i !== 1  ? this.state.inverted : !this.state.inverted} setActiveHex={this.setActiveHex} key={i} column={item.column} />)
+      <HexRow inverted={i !== 1  ? this.state.inverted : !this.state.inverted}
+              setActiveHex={this.setActiveHex} key={i} column={item.column}
+              toggleModal={this.toggleModal} />)
     
     return (
       <div className='App'>
@@ -37,6 +53,11 @@ class App extends React.Component {
             {hexRow}
           </div>
         </header>
+        {this.state.showModal &&
+          <div className="modal-wrapper">
+            <Modal closeModal={this.toggleModal} item={this.state.activeInfo} icon={this.state.icon}/>
+          </div>
+        }
       </div>
     )
   }
