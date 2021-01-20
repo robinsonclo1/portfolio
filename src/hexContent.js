f9256fd5ae1714c22489475e3a4e644d45537470
Robin <robin.clower@workstate.com>
2021-01-20 16:05:00 -0500

    better wording on my hex-content

--- src/hexContent.js
+++ src/hexContent.js
@@ -3,22 +3,16 @@ const hexContent = [
     column: [
       {
         id: 1,
-        title: "Accessibility",
+        title: "Accessibility Advocate",
         fontAwesomeIcon: "faWheelchair",
         embedUrl: "https://www.youtube.com/embed/BUCeHrSfp1M",
-        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at euismod ante. " +
-          "Vivamus bibendum augue ac tellus imperdiet pulvinar. Nullam varius euismod magna, sed pharetra massa " +
-          "feugiat eleifend. Vestibulum posuere cursus risus, vel rutrum felis tincidunt vel. Maecenas faucibus " +
-          "tellus ut ligula elementum imperdiet. Pellentesque sit amet eros sagittis, eleifend dui at, scelerisque " +
-          "libero. Sed venenatis dui at porttitor tempor. Donec vestibulum sapien vitae accumsan condimentum. Nullam eget " +
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at euismod ante. "
+        body: "After being introduced to the idea of Web Accessibility, Robin became the biggest proponent for it at his company. Every site he works on he strives to add in as many accessible features as possible. He was specifically hired by a broadcasting giant to revamp accessibility on one of their sites sued for a lack of ADA compliance. In the year he contracted with them, he closed 100+ tickets to bring the site into compliance by their end of year legal deadline. Robin loves making sites easier to navigate for everyone, whether they have a disability or not. He enjoys accessibility so much he even was selected to give a talk on it at Ohio Linux Fest 2020 entitled \"One Sprint to Drastically Improve Web Accessibility\", embedded below."
       },
       {
         id: 2,
-        title: "Hobbies",
+        title: "Hobbyist",
         fontAwesomeIcon: "faDice",
-        subhead: "subhead",
-        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at euismod ante. Vivamus bibendum augue ac tellus imperdiet pulvinar. Nullam varius euismod magna, sed pharetra massa feugiat eleifend. Vestibulum posuere cursus risus, vel rutrum felis tincidunt vel. Maecenas faucibus tellus ut ligula elementum imperdiet. Pellentesque sit amet eros sagittis, eleifend dui at, scelerisque libero. Sed venenatis dui at porttitor tempor. Donec vestibulum sapien vitae accumsan condimentum. Nullam eget dapibus odio, quis sodales odio. Curabitur egestas cursus ipsum, sed dapibus arcu mollis quis. Etiam commodo facilisis felis ut mollis. Suspendisse faucibus felis vitae sapien ultrices, a suscipit quam commodo. Integer ac eleifend odio."
+        body: "Robin loves a good work-life balance because of the multitude of hobbies he has outside of work. He's big into video games, especially Overwatch and Tetris. He loves board games, especially Catan (which was the original design inspiration for this site) and Gloomhaven. He's also an avid runner, having run 2 marathons, and loves outdoor activities like snowboarding, backpacking, and sports leagues. After recently buying a house he's also become enamoured with home improvement projects such as retiling the laundry room, installing uplighting, and creating a basement woodshop."
       }
     ]
   },
@@ -29,13 +23,11 @@ const hexContent = [
         title: "Educator",
         fontAwesomeIcon: "faMicrophoneAlt",
         embedUrl: "https://www.youtube.com/watch?v=8Qn4fTQdErQ",
-        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at euismod ante. " +
-          "Vivamus bibendum augue ac tellus imperdiet pulvinar. Nullam varius euismod magna, sed pharetra massa " +
-          "feugiat eleifend. Vestibulum posuere cursus risus, vel rutrum felis tincidunt vel. Maecenas faucibus "
+        body: "Robin went to college to become a high school math teacher, but after working in that environment he decided to pursue other interests. However his lifelong love for learning and teaching persists. While at Workstate, he founded the Shop Talk initiative, which gives monthly peer led talks ranging from the advent of C++, unconscious biases in our daily lives, and devops at one of our largest clients. In the two years shoptalk has been running, there have been talks every single month except March 2020 (due to obvious problems). Robin personally has given two shop talks, one about how to stay motivated in the workplace, and one about how to integrate Junior Developers into your team. He expanded this talk into an hour long seminar which he presented at both Ohio Linux Fest 2019 and Stir Trek 2019, where it was seen by hundreds of attendees."
       },
       {
         id: 4,
-        title: "Versatile",
+        title: "Leader",
         fontAwesomeIcon: "faToolbox",
         subhead: "subhead",
         body: "body"
@@ -49,14 +41,14 @@ const hexContent = [
         title: "Contact Me",
         fontAwesomeIcon: "faAddressCard",
         subhead: "subhead",
-        body: "body"
+        body: "Robin is most accessible via email at Robinson.Clower@gmail.com"
       },
       {
         id: 6,
-        title: "Technology",
+        title: "Technologist",
         fontAwesomeIcon: "faCogs",
         subhead: "subhead",
-        body: "body"
+        body: "As a consultant, Robin has worked on a wide variety of projects in his time at Workstate. His initial project was migrating several sites to Drupal 8 sites for a television network and did much of the front end development and theming. He then worked in liferay creating a site for a school in New York, followed by a stint in DevOps using Jenkins to spin up AWS resources, run extensive AI search algorithms and tear down the resources on completion. He was then hired back into the drupal land and shored up the accessibility of a TV show's site, followed by developing a new site using a drupal backend and a react frontend. Following his experience making the react site, Robin decided to try and get better at react, which led to the creation of this portfolio site made entirely in React."
       }
     ]
   }
